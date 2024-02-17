e414946ea4a587fdc8df7476770f13e870c80499
Stanislav <stanich81@gmail.com>
2024-02-17 20:51:56 +0300

    ОJформили в программе блок приёма строчного массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+// Задаем массив из строк
+Console.WriteLine("Задайте количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[n];
+Console.WriteLine($"Заполните массив на {n} элементов.");
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine($"Введите {i + 1}-й элемент: ");
+    array[i] = Console.ReadLine();
+}
+
+
+Console.WriteLine($"Выводим наш массив: ");
+Console.WriteLine(string.Join("; ", array));
+
